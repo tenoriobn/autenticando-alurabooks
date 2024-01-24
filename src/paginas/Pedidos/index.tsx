918eba3c2c02d4d7b4cfb072314cee32d08471df
Bruno Tenório <tenoriobn@gmail.com>
2024-01-24 18:41:10 -0300

    Adicionando funcionalidade de excluir pedidos

--- src/paginas/Pedidos/index.tsx
+++ src/paginas/Pedidos/index.tsx
@@ -19,6 +19,18 @@ const Pedidos = () => {
     .catch(erro => console.log(erro))
   }, []);
 
+  const excluir = (pedido: IPedido) => {
+    const token = sessionStorage.getItem('token')
+    axios.delete('http://localhost:8000/pedidos/' + pedido.id, {
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    }).then(() => {
+        setPedidos(pedidos.filter(p => p.id !== pedido.id))
+    })
+    .catch(erro => console.log(erro))
+}
+
   return (
     <section className="pedidos">
       <h1>Pedidos</h1>
@@ -29,6 +41,9 @@ const Pedidos = () => {
             <li>Data do pedido: <strong>{new Date(pedido.data).toLocaleDateString()}</strong></li>
             <li>Valor total: <strong>{formatador.format(pedido.total)}</strong></li>
             <li>Entrega realizada em: <strong>{new Date(pedido.entrega).toLocaleDateString()}</strong></li>
+            <li>
+                    <button onClick={() => excluir(pedido)}>Excluir</button>
+                </li>
           </ul>
           <AbBotao texto="Detalhes" />
         </div>
