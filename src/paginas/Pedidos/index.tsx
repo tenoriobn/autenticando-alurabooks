d7152117978de0cc1494f436507dc99319c476ba
Bruno Tenório <tenoriobn@gmail.com>
2024-01-24 18:08:59 -0300

    Requisição autenticadas

--- src/paginas/Pedidos/index.tsx
+++ src/paginas/Pedidos/index.tsx
@@ -1,7 +1,21 @@
 import { AbBotao } from "ds-alurabooks";
+import axios from "axios";
 import './Pedidos.css';
+import { useEffect } from "react";
+import { IPedido } from "../../interfaces/IPedido";
 
 const Pedidos = () => {
+  useEffect(() => {
+    const token = sessionStorage.getItem('token');
+
+    axios.get<IPedido[]>('http://localhost:8000/pedidos', {
+      headers: {
+        'Authorization': `Bearer ${token}`
+      }
+    }).then(resposta => console.log(resposta.data))
+    .catch(erro => console.log(erro))
+  }, []);
+
   return (
     <section className="pedidos">
       <h1>Pedidos</h1>
