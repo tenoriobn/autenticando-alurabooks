186faa7b04a595843d2c1a731f94d9583a3c07c5
Bruno Tenório <tenoriobn@gmail.com>
2024-01-24 18:34:52 -0300

    Exibindo pedidos

--- src/paginas/Pedidos/index.tsx
+++ src/paginas/Pedidos/index.tsx
@@ -1,10 +1,13 @@
 import { AbBotao } from "ds-alurabooks";
 import axios from "axios";
 import './Pedidos.css';
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { IPedido } from "../../interfaces/IPedido";
 
 const Pedidos = () => {
+  const [pedidos, setPedidos] = useState<IPedido[]>([]);
+  const formatador = Intl.NumberFormat('pt-br', { style: 'currency', currency: 'BRL' });
+
   useEffect(() => {
     const token = sessionStorage.getItem('token');
 
@@ -12,22 +15,24 @@ const Pedidos = () => {
       headers: {
         'Authorization': `Bearer ${token}`
       }
-    }).then(resposta => console.log(resposta.data))
+    }).then(resposta => setPedidos(resposta.data))
     .catch(erro => console.log(erro))
   }, []);
 
   return (
     <section className="pedidos">
       <h1>Pedidos</h1>
-        <div className="pedido">
+      {pedidos.map(pedido => (
+        <div className="pedido" key={pedido.id}>
           <ul>
-            <li>Pedido: <strong>123456</strong></li>
-            <li>Data do pedido: <strong>12/09/2022</strong></li>
-            <li>Valor total: <strong>R$ 42.00</strong></li>
-            <li>Entrega realizada em: <strong>13/09/2022</strong></li>
+            <li>Pedido: <strong>{pedido.id}</strong></li>
+            <li>Data do pedido: <strong>{new Date(pedido.data).toLocaleDateString()}</strong></li>
+            <li>Valor total: <strong>{formatador.format(pedido.total)}</strong></li>
+            <li>Entrega realizada em: <strong>{new Date(pedido.entrega).toLocaleDateString()}</strong></li>
           </ul>
           <AbBotao texto="Detalhes" />
-          </div>
+        </div>
+      ))}
     </section>
   );
 }
