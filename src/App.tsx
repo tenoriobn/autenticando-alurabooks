ca0af4ce47b569bfb933aa7aa2924edb8c99f144
Bruno Tenório <tenoriobn@gmail.com>
2024-01-25 16:29:34 -0300

    Concluindo desafio de interceptar resposta

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { unstable_HistoryRouter as HistoryRouter } from 'react-router-dom';
 import './App.css';
 import Rotas from './rotas';
 
+import { createBrowserHistory } from 'history';
+
+export const history = createBrowserHistory({window});
+
 function App() {
-  return (<BrowserRouter>
+  return (<HistoryRouter history={history}>
       <Rotas />
-    </BrowserRouter>
+    </HistoryRouter>
   );
 }
 
--- src/http/index.ts
+++ src/http/index.ts
@@ -1,4 +1,5 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
+import { history } from "../App";
 
 const http = axios.create({
   baseURL: 'http://localhost:8000',
@@ -9,7 +10,6 @@ const http = axios.create({
 });
 
 http.interceptors.request.use(function (config) {
-  // Do something before request is sent
   const token = sessionStorage.getItem("token");
   if (token && config.headers) {
     config.headers.Authorization = `Bearer ${token}`
@@ -17,8 +17,17 @@ http.interceptors.request.use(function (config) {
 
   return config;
 }, function (error) {
-  // Do something with request error
   console.log('Erro no interceptor do axios');
+  return Promise.reject(error);
+});
+
+http.interceptors.response.use(function (response) {
+  return response;
+}, function (error: AxiosError) {
+  if (error.response?.status === 401) {
+    history.push('/');
+    return Promise.reject()
+  }
   return Promise.reject(error);
 });
 
